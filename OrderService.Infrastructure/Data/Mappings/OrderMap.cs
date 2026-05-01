495314b1862f711913fc832f142d3e1aac38aca7
Leonardo Silva <leofla@gmail.com>
2026-05-01 12:15:04 -0300

    feat: add EF ProductMap e OrderMap pras entidades de domínio

---
+++ OrderService.Infrastructure/Data/Mappings/OrderMap.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Infrastructure.Data.Mappings;
+
+public class OrderMap : IEntityTypeConfiguration<Order>
+{
+  public void Configure(EntityTypeBuilder<Order> builder)
+  {
+    builder.ToTable("Orders");
+
+    builder.HasKey(o => o.Id);
+
+    builder.Property(o => o.CustomerId)
+        .IsRequired();
+
+    builder.Property(o => o.Currency)
+        .IsRequired()
+        .HasMaxLength(3); 
+
+    builder.Property(o => o.Total)
+        .HasPrecision(18, 2);
+
+    builder.Property(o => o.Status)
+        .HasConversion<string>()
+        .IsRequired();
+
+    builder.Metadata.FindNavigation(nameof(Order.Items))!
+        .SetPropertyAccessMode(PropertyAccessMode.Field);
+
+    builder.OwnsMany(o => o.Items, i =>
+    {
+      i.ToTable("OrderItems");
+      i.WithOwner().HasForeignKey("OrderId");
+
+      i.Property<Guid>("Id").IsRequired(); 
+      i.HasKey("Id");
+
+      i.Property(x => x.ProductId).IsRequired();
+      i.Property(x => x.Quantity).IsRequired();
+      i.Property(x => x.UnitPrice).HasPrecision(18, 2).IsRequired();
+    });
+  }
+}
---
+++ OrderService.Infrastructure/Data/Mappings/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Domain.Entities;
+
+public class ProductMap : IEntityTypeConfiguration<Product>
+{
+  public void Configure(EntityTypeBuilder<Product> builder)
+  {
+    builder.ToTable("Products");
+
+    builder.HasKey(p => p.Id);
+
+    builder.Property(p => p.UnitPrice)
+        .IsRequired()
+        .HasPrecision(18, 2); 
+
+    builder.Property(p => p.AvailableQuantity)
+        .IsRequired();
+  }
+}
