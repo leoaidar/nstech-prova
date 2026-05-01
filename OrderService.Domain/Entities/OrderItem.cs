c9a2e7483d32b5b03fffbc7d4e55930d5db19d57
Leonardo Silva <leofla@gmail.com>
2026-05-01 10:46:10 -0300

    feat: add Product e OrderItem

---
+++ OrderService.Domain/Entities/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using OrderService.Domain.Exceptions;
+
+namespace OrderService.Domain.Entities;
+
+public class OrderItem
+{
+  public Guid ProductId { get; private set; }
+  public decimal UnitPrice { get; private set; }
+  public int Quantity { get; private set; }
+
+  protected OrderItem() { }
+}
---
+++ OrderService.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using OrderService.Domain.Exceptions;
+
+namespace OrderService.Domain.Entities;
+
+public class Product
+{
+  public Guid Id { get; private set; }
+  public decimal UnitPrice { get; private set; }
+  public int AvailableQuantity { get; private set; }
+
+  protected Product() { }
+}
