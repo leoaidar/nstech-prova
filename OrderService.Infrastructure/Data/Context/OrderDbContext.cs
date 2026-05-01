d4cd5922f7cfe120f5555d7d42dd8a01314c4c57
Leonardo Silva <leofla@gmail.com>
2026-05-01 12:04:32 -0300

    feat: contexto EF banco OrderDB

---
+++ OrderService.Infrastructure/Data/Context/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Infrastructure.Data.Context;
+
+public class OrderDbContext : DbContext
+{
+  public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+  {
+  }
+
+  public DbSet<Order> Orders => Set<Order>();
+  public DbSet<Product> Products => Set<Product>();
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderDbContext).Assembly);
+
+    base.OnModelCreating(modelBuilder);
+  }
+}
