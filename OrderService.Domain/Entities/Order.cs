6a51c0d17dc15e261972c08bace0a84d462a67f7
Leonardo Silva <leofla@gmail.com>
2026-05-01 10:58:32 -0300

    feat: add entidade Order

---
+++ OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using OrderService.Domain.Enums;
+
+namespace OrderService.Domain.Entities;
+
+public class Order
+{
+  public Guid Id { get; private set; }
+  public Guid CustomerId { get; private set; }
+  public OrderStatus Status { get; private set; }
+  public string Currency { get; private set; } = string.Empty;
+  public decimal Total { get; private set; }
+  public DateTime CreatedAt { get; private set; }
+
+  private readonly List<OrderItem> _items = new();
+  public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
+
+  // Construtor protegido para uso do ORM(EF)
+  protected Order() { }
+}
