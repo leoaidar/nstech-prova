060a2db32304b4b453b6a790e835129443f5d572
Leonardo Silva <leofla@gmail.com>
2026-05-01 12:29:44 -0300

    feat: D do SOLID aplicando injeção de dependencia na infraestrutura

---
+++ OrderService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderService.Infrastructure.Data.Context;
+
+namespace OrderService.Infrastructure;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddDbContext<OrderDbContext>(options =>
+        options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+    return services;
+  }
+}
