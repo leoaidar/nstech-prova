c4828bc8372ccd8c4fc1af9b3bae9583a1b4e892
Leonardo Silva <leofla@gmail.com>
2026-05-01 22:50:23 -0300

    feat: adicionando a configuracao JWT para que a API saiba validar o token.

--- OrderService.Api/Program.cs
+++ OrderService.Api/Program.cs
@@ -1,6 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using OrderService.Application;
-using OrderService.Infrastructure; 
-
+using OrderService.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +13,35 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Register layers.
-builder.Services.AddApplication(); 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+
+// Register authentication.
+var jwtKey = builder.Configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT Key is missing");
+var keyBytes = Encoding.ASCII.GetBytes(jwtKey);
+
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+  options.RequireHttpsMetadata = false;
+  options.SaveToken = true;
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+    ValidateIssuer = true,
+    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+    ValidateAudience = true,
+    ValidAudience = builder.Configuration["Jwt:Audience"],
+    ValidateLifetime = true,
+    ClockSkew = TimeSpan.Zero
+  };
+});
+
 
 var app = builder.Build();
 
@@ -24,7 +53,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
